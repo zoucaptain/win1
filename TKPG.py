7415ebe7c7c8a39430b5c9df8b0958a1c1507320
zoucaptain <273971005@qq.com>
2018-07-10 21:49:42 +0800

    another try
    Signed-off-by: zoucaptain <273971005@qq.com>

---
+++ TKPG.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+# _*_ coding:utf8 _*_
+'''TKinter program
+    Module Tkinter Test
+'''
+import Tkinter
+
+top = Tkinter.Tk()
+label = Tkinter.Label(top, text='Hello world')
+label.pack()
+Tkinter.mainloop()
